147127e7269656faab05a9e44478ee73719b4aea
gabrielbigliardi <gabrielbigliardi@gmail.com>
2024-07-01 20:03:41 -0300

    feat: add new supplier

--- src/app/suppliers/suppliers-new/suppliers-new.component.ts
+++ src/app/suppliers/suppliers-new/suppliers-new.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core'
+import { SuppliersFormComponent } from '../suppliers-form/suppliers-form.componen\
+t'
+import { MaterialModule } from '../../material.module'
+import { Observable, lastValueFrom, of } from 'rxjs'
+import { Router } from '@angular/router'
+import { SupplierService } from '../supplier.service'
+import { AsyncPipe } from '@angular/common'
+import { LoadingBarComponent } from '../../loading-bar.component'
+import { Supplier } from '../suppliers/supplier.dto'
 @Component({
   selector: 'app-suppliers-new',
   standalone: true,
-  imports: [],
+  imports: [
+    MaterialModule,
+    SuppliersFormComponent,
+    AsyncPipe,
+    LoadingBarComponent
+  ],
   templateUrl: './suppliers-new.component.html',
   styles: ``
 })
 export class SuppliersNewComponent {
+  router = inject(Router)
+  supplierService = inject(SupplierService)
+  supplierObservable!: Observable<Supplier>
+  supplier: Supplier
 
+  async ngOnInit() {
+    this.supplierObservable = await of(this.supplierService.create())
+    this.supplier = await lastValueFrom(this.supplierObservable)
+  }
+  async onSave(supplier: Supplier) {
+    console.log('SALVANDO');
+
+    this.supplierObservable = this.supplierService.save(supplier)
+    const result = await lastValueFrom(this.supplierObservable)
+    this.router.navigate(['/suppliers/show', result.id])
+  }
+  onBack() {
+    this.router.navigate(['/suppliers']);
+  }
 }
