b0c9891f627f788763a5832a6c4ab460db5b36c8
gabrielbigliardi <gabrielbigliardi@gmail.com>
2024-07-01 19:20:17 -0300

    feat: suppliers:id / edit supplier

--- src/app/suppliers/supplier.service.ts
+++ src/app/suppliers/supplier.service.ts
@@ -18,4 +18,34 @@ export class SupplierService {
   public getById(id: Number): Observable<Supplier> {
     return this.http.get<Supplier>(environment.api + 'suppliers/' + id)
   }
+
+  public save(supplier: Supplier): Observable<Supplier> {
+    if (supplier.id)
+      return this.http.put<Supplier>(
+        environment.api + 'suppliers/' + supplier.id,
+        supplier
+      )
+    return this.http.post<Supplier>(environment.api + 'suppliers', supplier)
+  }
+
+  public delete(id?: number): Observable<Supplier> {
+    return this.http.delete<Supplier>(environment.api + 'suppliers/' + id)
+  }
+
+  public create(): Supplier {
+    return {
+      id: 0,
+      companyName: '',
+      contactName: '',
+      contactTitle: '',
+      address: {
+        city: '',
+        phone: '',
+        country: '',
+        postalCode: 0,
+        region: '',
+        street: ''
+      }
+    }
+  }
 }
--- src/app/suppliers/suppliers-edit/suppliers-edit.component.ts
+++ src/app/suppliers/suppliers-edit/suppliers-edit.component.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { SupplierService } from '../supplier.service';
+import { Supplier } from '../suppliers/supplier.dto';
+import { Observable, lastValueFrom } from 'rxjs';
+import { LoadingBarComponent } from '../../loading-bar.component';
+import { MaterialModule } from '../../material.module';
+import { AsyncPipe } from '@angular/common';
+import { SuppliersFormComponent } from '../suppliers-form/suppliers-form.component';
 
 @Component({
   selector: 'app-suppliers-edit',
   standalone: true,
-  imports: [],
+  imports: [LoadingBarComponent, MaterialModule, AsyncPipe, RouterLink, SuppliersFormComponent],
   templateUrl: './suppliers-edit.component.html',
   styles: ``
 })
-export class SuppliersEditComponent {
+export class SuppliersEditComponent implements OnInit {
+  route = inject(ActivatedRoute)
+  supplierService = inject(SupplierService)
+  supplier: Supplier
+  supplierObservable: Observable<Supplier>
+
+  constructor(private router: Router) { }
+
+
+  async ngOnInit() {
+    const id: Number = +(this.route.snapshot.paramMap.get('id') || 0)
+    this.supplierObservable = this.supplierService.getById(id)
+    this.supplier = await lastValueFrom(this.supplierObservable)
+    console.log(this.supplier)
+  }
+
+
+
+  async onSave(supplier: Supplier) {
+    this.supplierObservable = this.supplierService.save(supplier);
+    this.supplier = await lastValueFrom(this.supplierObservable);
+    this.router.navigate(['/suppliers/show/', supplier?.id]);
+  }
+  onBack() {
+    this.router.navigate(['/suppliers']);
+  }
+
+
+
+
+  // async ngOnInit() {
+  //   const id: Number = +(this.route.snapshot.paramMap.get('id') || 0)
+  //   this.supplierObservable = this.supplierService.getById(id)
+  //   this.supplier = await lastValueFrom(this.supplierObservable)
+  //   console.log(this.supplier)
+  // }
+
+  // async onSave(supplier: Supplier) {
+  //   this.supplierObservable = this.supplierService.save(supplier);
+  //   this.supplier = await lastValueFrom(this.supplierObservable);
+  //   this.router.navigate(['/suppliers/show', supplier?.id])
+  //   // this.router.navigate(['/suppliers/show/',supplier?.id]);
+  // }
+  // onBack() {
+  //   this.router.navigate(['/suppliers']);
+  // }
 
 }
